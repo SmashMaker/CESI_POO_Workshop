ba90a857d513accc5628e3a823ce05c83900c171
SmashMakerV <unknown@null.com>
2023-11-21 22:32:28 +0100

    Résolution du 1 : Ajout d'un pointeur null à chaque constructeur

--- Objet2D.cpp
+++ Objet2D.cpp
@@ -8,16 +8,19 @@
 Objet2D::Objet2D(void) {
     this->x = 0;
     this->y = 0;
+    this->Suivant = nullptr;
 }
 
 Objet2D::Objet2D(int x) {
     this->x = x;
     this->y = 0;
+    this->Suivant = nullptr;
 }
 
 Objet2D::Objet2D(int x, int y) {
     this->x = x;
     this->y = y;
+    this->Suivant = nullptr;
 }
 
 // Setters & Getters
