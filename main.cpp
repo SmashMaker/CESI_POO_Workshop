f67237f1bb118e75f0543162539fb10ac89fdd83
SmashMakerV <paullegall0502@gmail.com>
2023-11-23 14:21:58 +0100

    Changement du main : resolution séparé en fonction et création d'une v2 de la résolution 2 car chaineobjet2d mauvais

--- main.cpp
+++ main.cpp
@@ -9,10 +9,12 @@
 #include "Carre.h"
 
 #include "ChainObjet2D.h"
+#include "NewChainObjet2D.h"
 
-int main(){
+
+void resolution1(){
     // Test carré et rectangle
-    std::cout << "Debut du programme" << std::endl;
+    std::cout << "#### Debut du programme ####" << std::endl;
     Objet2D *obj1 = new Rectangle(5, 10);
     std::cout << obj1->afficheInfo() << std::endl;
 
@@ -21,29 +23,91 @@ int main(){
 
 
     // Test pointeur Suivant
-    std::cout << std::endl << "Partie 1" << std::endl;
+    std::cout << std::endl << "#### Partie 1 ####" << std::endl;
 
     obj1->setSuivant(obj2);
     obj1->getSuivant()->setX(10);
 
     std::cout << obj1->getSuivant()->afficheInfo() << std::endl;
     std::cout << "Value is supposed to be 0 : " <<
-    obj2->getSuivant() << std::endl;
+              obj2->getSuivant() << std::endl;
+    delete obj1, obj2;
+}
 
-    // Test chain
-    std::cout << std::endl << "Partie 2" << std::endl;
+void resolution2(){
+    std::cout << std::endl << "#### Partie 2 ####" << std::endl;
 
-    obj1->setSuivant(nullptr); // Reset link
+    Objet2D* obj1 = new Rectangle(5, 10);
+    Objet2D* obj2 = new Carre(5);
+    Objet2D* obj3 = new Rectangle(10, 10);
 
     ChainObjet2D *chain = new ChainObjet2D(obj1);
     chain->checkIfAllGood();
-
     chain->printChain();
-    chain->addAtTail(obj2);
+
+
+    std::cout << "Ajout et Suppr Head" << std::endl;
+    chain->addAtHead(obj2);
+    chain->printChain();
+    chain->removeHead();
+    chain->printChain();
     chain->checkIfAllGood();
+
+    std::cout << "Ajout et Suppr Tail" << std::endl;
+    chain->addAtTail(obj2);
+    chain->printChain();
+    chain->removeTail();
+    chain->printChain();
+    chain->checkIfAllGood();
+
+    std::cout << "Ajout et Suppr Index" << std::endl;
+    chain->addAtTail(obj2);
+    chain->addAtIndex(obj3, 1);
+    chain->printChain();
+    chain->removeFromIndex(1);
+    chain->printChain();
+    chain->checkIfAllGood();
+
+    delete obj1,obj2,obj3;
+    delete chain;
+}
+
+void resolution2v2(){
+    std::cout << std::endl << "#### Partie 2 ####" << std::endl;
+
+    Objet2D* obj1 = new Rectangle(5, 10);
+    Objet2D* obj2 = new Carre(5);
+    Objet2D* obj3 = new Rectangle(10, 10);
+
+    NewChainObjet2D *chain = new NewChainObjet2D(obj1);
     chain->printChain();
 
-    delete obj1, obj2;
+    std::cout << "Ajout et suppression de la head" << std::endl;
+    chain->addAtHead(obj2);
+    chain->printChain();
+    chain->removeHead();
+    chain->printChain();
+
+    std::cout << std::endl << "Ajout et suppression de la tail" << std::endl;
+    chain->addAtTail(obj2);
+    chain->printChain();
+    chain->removeTail();
+    chain->printChain();
+
+    std::cout << std::endl << "Ajout et suppression par index" << std::endl;
+    chain->addAtTail(obj2);
+    chain->printChain();
+    chain->addAtIndex(obj3, 1);
+    chain->printChain();
+    chain->removeFromIndex(1);
+    chain->printChain();
+
+    delete obj1,obj2,obj3;
     delete chain;
+}
+
+
+int main(){
+    resolution2v2();
     return 0;
 }
